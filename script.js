bc6726f5d4fc22d6774c42b4dd946946a1cf2843
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-01-13 19:23:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,18 +7,17 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
-const team=players;
+/*const team=players;
 const team1=[...players];
 const cap1=[...person];
-
-/*const team = players;
+*/
+const team = players;
 
 // Create a copy of the array players
 const team1 = players.slice();
 
 // Create a copy of the object person
 const cap1 = Object.assign({}, person);
-*/
 
 
 
